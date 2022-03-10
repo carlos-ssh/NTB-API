d888cac8c8cb1e198e327975c58cb48f5362bd30
Carlos <carlos.anezeh@gmail.com>
2022-03-09 22:27:13 -0600

    Create a JsonHelper

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,27 +2,24 @@ require 'rails_helper'
 
 describe ArticlesController do
   describe '#index' do
+    subject { get :index }
     it 'should return success response' do
-      get :index
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'should return proper json' do
-      create_list :article, 2
-      get :index
+      articles = create_list :article, 5
+      subject
       json = JSON.parse(response.body)
       json_data = json['data']
-      expect(json_data.length).to eq(2)
-      expect(json_data[0]['attributes']).to eq({
-        'title' => 'Title 1',
-        'content' => 'My text content 1',
-        'slug' => 'ak-47-1'
-      })
-      expect(json_data[1]['attributes']).to eq({
-        'title' => 'Title 2',
-        'content' => 'My text content 2',
-        'slug' => 'ak-47-2'
-      })
+      articles.each_with_index do |article, index|
+        expect(json_data[index]['attributes']).to eq({
+          'title' => article.title,
+          'content' => article.content,
+          'slug' => article.slug
+        })
+      end
     end
   end
 end
