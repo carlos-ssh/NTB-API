22233c7f69c645569bada7a243fcca275516cb4f
Carlos <carlos.anezeh@gmail.com>
2022-03-09 22:39:21 -0600

    Create scopes for latest articles.

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -9,17 +9,25 @@ describe ArticlesController do
     end
 
     it 'should return proper json' do
-      articles = create_list :article, 5
+      create_list :article, 5
       subject
       json = JSON.parse(response.body)
       json_data = json['data']
-      articles.each_with_index do |article, index|
+      Article.recent.each_with_index do |article, index|
         expect(json_data[index]['attributes']).to eq({
           'title' => article.title,
           'content' => article.content,
           'slug' => article.slug
         })
       end
+    end
+
+    it 'should return articles in proper order' do
+      old_article = create :article
+      newer_article = create :article
+      subject
+      expect(json_data.first['id']).to eq(newer_article.id.to_s)
+      expect(json_data.last['id']).to eq(old_article.id.to_s)
     end
   end
 end
